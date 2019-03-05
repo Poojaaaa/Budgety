c276d21161e6c4feae9bad076eebd003d33889c5
Poojaaaa <Pooja.gajera11@gmail.com>
2019-03-04 20:58:02 -0800

    Adding new item to budget controller

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
-
-  var Expense = function(id, description, value){
+  var Expense = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
-  var Income = function(id, description, value){
+  var Income = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
@@ -22,8 +21,37 @@ var budgetController = (function() {
       exp: 0,
       inc: 0
     }
-  }
+  };
 
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      // Create a new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create a new item based on 'inc' or 'exp' type
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push it into data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
+    }
+  };
 })();
 
 // UI CONTROLLER
@@ -65,10 +93,14 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
+    var input, newItem;
+
     // 1. Get the filed input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
 
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
     // 3. Add the item to UI
     // 4. Calculate the budget
     // 5. Display the budget on UI
@@ -77,7 +109,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log("Application has started!");
-      setupEventListener()
+      setupEventListener();
     }
   };
 })(budgetController, UIController);
